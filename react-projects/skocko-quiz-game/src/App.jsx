6e1043581cb21f2c4c92d2d6a0fb082345047922
daniloPolaris <131767708+daniloPolaris@users.noreply.github.com>
2023-05-31 21:13:43 +0200

    coaching-app deleted, skosko-quiz-game created

---
+++ react-projects/skocko-quiz-game/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <p className='text-amber-600'>Nesto</p>
+      <button className='bg-slate-800 text-gray-400'>Dugme</button>
+      <span>da</span>
+    </>
+  )
+}
+
+export default App
