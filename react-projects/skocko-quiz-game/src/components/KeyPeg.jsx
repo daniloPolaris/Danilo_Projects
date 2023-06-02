cab31278e258cdd98897a479532368476a175b1e
daniloPolaris <131767708+daniloPolaris@users.noreply.github.com>
2023-06-02 20:56:02 +0200

    No z-index.

--- react-projects/skocko-quiz-game/src/components/KeyPeg.jsx
+++ react-projects/skocko-quiz-game/src/components/KeyPeg.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const KeyPeg = ({dot}) => {
   return (
-    <div className='h-10 w-10 bg-slate-200 rounded-full flex justify-center items-center -z-10'>{dot}</div>
+    <div className='h-10 w-10 bg-slate-200 rounded-full flex justify-center items-center'>{dot}</div>
   )
 }
 
