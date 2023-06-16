68380ef7ad70e34efa019a0511d156700c5e377f
daniloPolaris <131767708+daniloPolaris@users.noreply.github.com>
2023-06-16 14:15:33 +0200

    za nemanju

--- react-projects/kanban-board/src/pages/BoardPage.jsx
+++ react-projects/kanban-board/src/pages/BoardPage.jsx
@@ -22,17 +22,16 @@ function BoardPage() {
       setColaborators(parsedData[splitUrl].colaborators);
     }
   }, []);
-  
 
   const handleBoardSelect = (boardId) => {
     setBoardId(boardId);
     const selectedBoard = boardData[boardId];
     setColaborators(selectedBoard?.colaborators || []);
+    // ovde isto za taskove pisem
   };
-  
 
   const handleAddColaborator = () => {
-    setShowAddColaborator(true);
+    setShowAddColaborator(!showAddColaborator);
   };
 
   const handleColaboratorNameChange = (event) => {
@@ -41,8 +40,22 @@ function BoardPage() {
 
   const handleAddColaboratorToList = () => {
     if (colaboratorName.trim() !== "") {
-      setColaborators([...colaborators, colaboratorName]);
+      setColaborators(prevState => [...prevState, colaboratorName]);
       setColaboratorName("");
+      console.log('list');
+      const updatedBoardData = [...boardData];
+
+      console.log("Board data:",updatedBoardData);
+      // console.log("Updated Board data:",{updatedBoardData});
+      
+      updatedBoardData[boardId].colaborators = colaborators;
+      console.log("colaborators", colaborators);
+      setBoardData(updatedBoardData);
+  
+      console.log("Updated Board data:",updatedBoardData);
+  
+      localStorage.setItem("boardData", JSON.stringify(updatedBoardData));
+      console.log('storage');
     }
   };
 
@@ -50,17 +63,26 @@ function BoardPage() {
     const updatedColaborators = [...colaborators];
     updatedColaborators.splice(index, 1);
     setColaborators(updatedColaborators);
+
   };
 
   const handleColaboratorsData = () => {
-    const updatedBoardData = [ ...boardData ];
-    updatedBoardData[boardId].colaborators = colaborators;
-    setBoardData(updatedBoardData);
-    localStorage.setItem("boardData", JSON.stringify(updatedBoardData));
-    setShowAddColaborator(false);
-  }
+    const updatedBoardData = [...boardData];
 
-console.log(colaborators);
+    console.log("Board data:",updatedBoardData);
+    // console.log("Updated Board data:",{updatedBoardData});
+    
+    updatedBoardData[boardId].colaborators = colaborators;
+    console.log("colaborators", colaborators);
+    setBoardData(updatedBoardData);
+
+    console.log("Updated Board data:",updatedBoardData);
+
+    localStorage.setItem("boardData", JSON.stringify(updatedBoardData));
+    console.log('storage');
+  };
+
+  console.log(colaborators);
   return (
     <div className="flex flex-col h-full bg-blue-400">
       <header className="bg-slate-600">
@@ -74,11 +96,15 @@ console.log(colaborators);
         <div className="flex justify-between items-center max-w-7xl mx-auto my-0 py-1">
           <div className="flex items-center">
             <span className="text-2xl pb-1 mr-5">{boardData[boardId] && boardData[boardId].title}</span>
-            <div className="text-sm">
-              <span className="p-1 bg-sky-400 rounded-full mr-1 text-white">DS</span>
-              <span className="p-1 bg-sky-400 rounded-full mr-1 text-white">DS</span>
-              <span className="p-1 bg-sky-400 rounded-full mr-1 text-white">DS</span>
-              <span className="p-1 bg-sky-400 rounded-full mr-1 text-white">DS</span>
+            <div className="text-sm flex">
+              {colaborators && colaborators.map((colaborator, index) => (
+                <span className="p-1 bg-sky-400 flex items-center justify-center w-8 h-8 rounded-full mr-1 text-white" key={index}>
+                  {colaborator
+                    .split(" ")
+                    .map((name) => name[0].toUpperCase())
+                    .join("")}
+                </span>
+              ))}
             </div>
           </div>
           <div>
@@ -177,31 +203,30 @@ console.log(colaborators);
       </div>
       {showAddColaborator && (
         <div className="fixed top-24 left-0 right-0  flex justify-center">
-          <div className="bg-white p-4 rounded-md shadow-md">
-            <label htmlFor="colaboratorName" className="block mb-2">
+          <div className="bg-white p-4 rounded-md shadow-md flex flex-col">
+            <label htmlFor="colaboratorName" className="mb-3">
               Colaborator Name:
             </label>
-            <input
-              type="text"
-              id="colaboratorName"
-              className="border border-slate-400 focus:outline-none rounded-md p-1.5 mb-2"
-              value={colaboratorName}
-              onChange={handleColaboratorNameChange}
-            />
-            <button
-              className="bg-slate-400 text-white rounded px-3 py-2 ml-2 text-base"
-              onClick={handleAddColaboratorToList}
-            >
-              Add
-            </button>
-            {colaborators.length > 0 && (
-              <div className="mt-4">
+            <div className="flex items-center mb-8">
+              <input
+                type="text"
+                id="colaboratorName"
+                className="border border-slate-400 focus:outline-none rounded-md p-1.5"
+                value={colaboratorName}
+                onChange={handleColaboratorNameChange}
+              />
+              <button className="bg-slate-400 text-white rounded px-3 py-2 ml-2" onClick={handleAddColaboratorToList}>
+                Add
+              </button>
+            </div>
+            {colaborators && colaborators.length > 0 && (
+              <div className="mb-5">
                 {colaborators.map((colaborator, index) => (
-                  <div className="flex items-center" key={index}>
+                  <div className="flex items-center justify-between mb-2" key={index}>
                     <span>{colaborator}</span>
                     <button
-                      className="ml-2 p-1 bg-red-500 text-white rounded"
-                      onClick={() => handleRemoveColaborator(index)}
+                      className="p-1 bg-red-500 text-white rounded"
+                      onClick={() => {handleRemoveColaborator(index); handleColaboratorsData()}}
                     >
                       Remove
                     </button>
@@ -209,7 +234,11 @@ console.log(colaborators);
                 ))}
               </div>
             )}
-            <button onClick={handleColaboratorsData}>Finish</button>
+            <div className="flex justify-center">
+              <button className="bg-slate-400 py-2 px-8 rounded-md text-white" onClick={handleAddColaborator}>
+                Done
+              </button>
+            </div>
           </div>
         </div>
       )}
