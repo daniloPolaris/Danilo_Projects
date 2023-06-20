5e0d11d111988fc2eb03805f83ef06dba9a3c2c8
daniloPolaris <131767708+daniloPolaris@users.noreply.github.com>
2023-06-20 02:11:03 +0200

    DnD revision completed.

--- react-projects/kanban-board/src/pages/BoardPage.jsx
+++ react-projects/kanban-board/src/pages/BoardPage.jsx
@@ -97,6 +97,10 @@ function BoardPage() {
     }
   };
 
+  const generateUniqueID = () => {
+    return Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
+  };
+
   const handleSaveButtonClick = (event) => {
     event.preventDefault();
 
@@ -106,7 +110,8 @@ function BoardPage() {
     // setColumns(boardData[boardId].columns)
 
     const newTask = {
-      id: columns[0]?.tasks?.length + 1 || 1,
+      // id: columns[0]?.tasks?.length + 1 || 1,
+      id: generateUniqueID(),
       title: title,
       storyPoints: storyPoints,
       description: description,
@@ -129,26 +134,24 @@ function BoardPage() {
       setShowCreateTask(false);
     }
   };
-  
+
   const handleDragEnd = (result) => {
     const { source, destination } = result;
     if (!destination) return;
-  
+
     const updatedBoardData = [...boardData];
     const sourceColumnIndex = parseInt(source.droppableId);
     const destinationColumnIndex = parseInt(destination.droppableId);
     const sourceColumn = updatedBoardData[boardId].columns[sourceColumnIndex];
     const destinationColumn = updatedBoardData[boardId].columns[destinationColumnIndex];
-  
-    if (sourceColumn && sourceColumn.tasks && sourceColumnIndex !== updatedBoardData[boardId].columns.length - 1) {
 
+    if (sourceColumn && sourceColumn.tasks && sourceColumnIndex !== updatedBoardData[boardId].columns.length - 1) {
       const [removed] = sourceColumn.tasks.splice(source.index, 1);
       destinationColumn.tasks.splice(destination.index, 0, removed);
 
       setBoardData(updatedBoardData);
       localStorage.setItem("boardData", JSON.stringify(updatedBoardData));
     }
-
   };
 
   return (
@@ -201,52 +204,48 @@ function BoardPage() {
             {boardData[boardId] &&
               boardData[boardId].columns &&
               boardData[boardId].columns.map((column) => (
-                <Droppable droppableId={column.id} key={column.id}>
-                  {(provided) => (
-                    <div
-                      className="flex-grow bg-slate-300 rounded shadow-md flex-1"
-                      key={column.id}
-                      ref={provided.innerRef}
-                      {...provided.droppableProps}
-                    >
-                      <div className="flex justify-center bg-slate-400 text-slate-800 font-semibold rounded  mb-4 py-2 shadow-md">
-                        {column.name}
-                      </div>
-                      {column.tasks &&
-                        column.tasks.map((task, taskIndex) => (
-                          <Draggable draggableId={`${task.id}`} index={taskIndex} key={task.id}>
-                            {(provided) => (
-                              <div
-                                className="bg-white flex justify-between items-center rounded mb-2 py-4 mx-1 shadow-md"
-                                key={task.id}
-                                ref={provided.innerRef}
-                                {...provided.draggableProps}
-                                {...provided.dragHandleProps}
-                              >
-                                <span className="ml-2">{task.title}</span>
-                                <div className="flex justify-end">
-                                  {task.assignedTo &&
-                                    task.assignedTo.map((collaborator, collaboratorIndex) => (
-                                      <span
-                                        className="p-1 bg-sky-400 w-7 h-7 flex justify-center items-center rounded-full mr-1 text-sm text-white"
-                                        key={collaboratorIndex}
-                                      >
-                                        {collaborator
-                                          .split(" ")
-                                          .map((name) => name[0].toUpperCase())
-                                          .join("")}
-                                      </span>
-                                    ))}
+                <div className="flex flex-col flex-grow bg-slate-300 rounded shadow-md flex-1" key={column.id}>
+                  <div className="flex justify-center bg-slate-400 text-slate-800 font-semibold rounded  mb-4 py-2 shadow-md">
+                    {column.name}
+                  </div>
+                  <Droppable droppableId={column.id} key={column.id}>
+                    {(provided) => (
+                      <div className="flex-grow" ref={provided.innerRef} {...provided.droppableProps}>
+                        {column.tasks &&
+                          column.tasks.map((task, taskIndex) => (
+                            <Draggable draggableId={task.id} index={taskIndex} key={task.id}>
+                              {(provided) => (
+                                <div
+                                  className="bg-white flex justify-between items-center rounded mb-2 py-4 mx-1 shadow-md"
+                                  key={task.id}
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                >
+                                  <span className="ml-2">{task.title}</span>
+                                  <div className="flex justify-end">
+                                    {task.assignedTo &&
+                                      task.assignedTo.map((collaborator, collaboratorIndex) => (
+                                        <span
+                                          className="p-1 bg-sky-400 w-7 h-7 flex justify-center items-center rounded-full mr-1 text-sm text-white"
+                                          key={collaboratorIndex}
+                                        >
+                                          {collaborator
+                                            .split(" ")
+                                            .map((name) => name[0].toUpperCase())
+                                            .join("")}
+                                        </span>
+                                      ))}
+                                  </div>
                                 </div>
-                              </div>
-                            )}
-                          </Draggable>
-                        ))}
-
-                      {provided.placeholder}
-                    </div>
-                  )}
-                </Droppable>
+                              )}
+                            </Draggable>
+                          ))}
+                        {provided.placeholder}
+                      </div>
+                    )}
+                  </Droppable>
+                </div>
               ))}
           </div>
         </DragDropContext>
